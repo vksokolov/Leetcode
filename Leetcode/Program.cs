ce17f83c2ac828f8313c16ed0fab812c50ab541e
Vladislav Sokolov <vladislav.k.sokolov@gmail.com>
2023-03-15 21:32:17 +0300

    Blank project improved

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,3 +1,23 @@
-﻿class Solution
+﻿using System;
+
+class Program
 {
+    public static void Main()
+    {
+        Solution s = new Solution();
+        Print(s.GetResult());
+    }
+
+    public static void Print(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
+class Solution
+{
+    public string GetResult()
+    {
+        return .ToString();
+    }
 }
