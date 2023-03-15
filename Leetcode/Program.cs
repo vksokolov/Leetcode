9074c07a4e3967050934dfd53b22756be593a5d8
Vladislav Sokolov <vladislav.k.sokolov@gmail.com>
2023-03-15 21:30:17 +0300

    9. Palindrome Number

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,3 +1,34 @@
-﻿class Solution
+﻿using System;
+
+class Program
 {
+    public static void Main()
+    {
+        Solution s = new Solution();
+        Print(s.GetResult());
+    }
+
+    public static void Print(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+class Solution
+{
+    public string GetResult()
+    {
+        return IsPalindrome(1000021).ToString();
+    }
+    
+    public bool IsPalindrome(int x)
+    {
+        if (x == 0) return true;
+        if (x < 0) return false;
+
+        var xStr = x.ToString();
+        for (int a = 0, b = xStr.Length - 1; a < b; a++, b--)
+            if (xStr[a] != xStr[b]) return false;
+
+        return true;
+    }
 }
