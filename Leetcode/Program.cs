b5350a9cae90666b1bb008425a5404394deb8ac9
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-03 23:37:07 +0300

    27. Remove Element

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,28 +1,43 @@
 ﻿using System;
 
-namespace Leetcode
+namespace Leetcode;
+
+class Program
 {
-    class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            var s = new Solution();
-            Print(s.GetResult());
-        }
-
-        private static void Print(string msg) =>
-            Console.WriteLine(msg);
+        var s = new Solution();
+        Print(s.GetResult());
     }
 
-    internal partial class Solution
-    {
-        public string GetResult()
-        {
-            return .ToString();
-        }
-    }
+    private static void Print(string msg) =>
+        Console.WriteLine(msg);
+}
 
-    internal partial class Solution
+internal partial class Solution
+{
+    public string GetResult()
     {
+        int[] nums = { 3,2,2,3 };
+        //int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 };
+        //int[] nums = { 1 };
+        //int[] nums = { 3, 3 };
+        var val = 3;
+        return RemoveElement(nums, val).ToPrettyString();
+    }
+}
+
+internal partial class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var cur = 0;
+        for (var i = 0; i < nums.Length;)
+        {
+            if (nums[i++] == val) continue;
+            
+            nums[cur++] = nums[i-1];
+        }
+        return cur;
     }
 }
