352ff4fcbfdda66e6fec98685b39738dc2ceee40
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-03 17:51:23 +0300

    14. Longest Common Prefix

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Leetcode
 {
@@ -18,11 +19,38 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToString();
+            string[] strs = { "flower", "flow", "flight" };
+            return LongestCommonPrefix(strs);
         }
     }
 
     internal partial class Solution
     {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var result = new StringBuilder("");
+            var charIndex = 0;
+            var maxIndex = int.MaxValue;
+            var strCount = strs.Length;
+
+            foreach (var str in strs)
+                if (str.Length < maxIndex)
+                    maxIndex = str.Length;
+
+            for (var i = 0; i < maxIndex; i++)
+            {
+                var firstStrChar = strs[0][charIndex];
+                for (var strIndex = 1; strIndex < strCount; strIndex++)
+                {
+                    var str = strs[strIndex];
+                    if (str[charIndex] != firstStrChar) return result.ToString();
+                }
+
+                charIndex++;
+                result.Append(firstStrChar);
+            }
+
+            return result.ToString();
+        }
     }
 }
