4c035ad6be4205b0972b08b9de4d444b6779ef28
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-03 19:43:32 +0300

    26. Remove Duplicates from Sorted Array

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Leetcode
 {
@@ -18,11 +19,31 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToString();
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            return RemoveDuplicates(nums).ToString();
         }
     }
 
     internal partial class Solution
     {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            
+            var index = 1;
+            var prevAddedValue = nums[0];
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (prevAddedValue != nums[i])
+                {
+                    nums[index] = nums[i];
+                    prevAddedValue = nums[i];
+                    index++;
+                }
+            }
+
+            return index;
+        }
     }
 }
