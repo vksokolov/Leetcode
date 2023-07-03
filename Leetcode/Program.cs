ca1fd2a78823cce276b0c4f4f8e487a1837c23dc
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-03 19:32:56 +0300

    21. Merge Two Sorted Lists

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -18,11 +18,55 @@ namespace Leetcode
     {
         public string GetResult()
         {
+            var result = MergeTwoLists()
             return .ToString();
         }
     }
 
     internal partial class Solution
     {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+         
+            ListNode result = list1;
+            if (list1.val > list2.val)
+            {
+                list1 = list2;
+                list2 = result;
+                result = list1;
+            }
+            ListNode cur = list1;
+            ListNode tmp;
+            
+            while (list2 != null)
+            {
+                if (cur.next == null || cur.next.val > list2.val)
+                {
+                    tmp = list2;
+                    list2 = list2.next;
+
+                    tmp.next = cur.next;
+                    cur.next = tmp;
+                    cur = tmp;
+                }
+                else
+                {
+                    cur = cur.next;
+                }
+            }
+
+            return result;
+        }
+    }
+    
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
