e9b7d065062dee0381061e9b0461def39bbf2653
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-05 17:06:47 +0300

    35. Search Insert Position

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -18,11 +18,43 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToPrettyString();
+            int[] arr = { 1, 3, 5 };
+            return SearchInsert(arr, 4).ToPrettyString();
         }
     }
 
     internal partial class Solution
     {
+        public int SearchInsert(int[] nums, int target) {
+            var leftBound = 0;
+            var l = nums.Length;
+            var rightBound = l;
+
+            while (leftBound < rightBound)
+            {
+                int i = (leftBound + rightBound) / 2;
+
+                if (nums[i] == target) return i;
+
+                if (nums[i] < target)
+                {
+                    if (i == l-1) return l;
+
+                    if (target < nums[i + 1]) return i + 1;
+
+                    leftBound = i + 1;
+                }
+                else
+                {
+                    if (i == 0) return 0;
+
+                    if (nums[i - 1] < target) return i;
+
+                    rightBound = i;
+                }
+            }
+
+            return nums.Length;
+        }
     }
 }
