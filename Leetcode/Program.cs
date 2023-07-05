6199e4365d7793e30001151f358f3f0904f8e522
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-05 18:03:59 +0300

    67. Add Binary

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Leetcode
 {
@@ -18,11 +19,40 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToPrettyString();
+            return AddBinary("1010", "1011").ToPrettyString();
         }
     }
 
     internal partial class Solution
     {
+        public string AddBinary(string a, string b)
+        {
+            if (a.Length < b.Length) (a, b) = (b, a);
+            
+            var sb = new StringBuilder();
+            var buffer = 0;
+            
+            for (int i = 1; i <= a.Length; i++)
+            {
+                int aC = a[^i] - '0';
+                int bC = b.Length >= i ? b[^i] - '0' : 0;
+                buffer += aC + bC;
+                if (buffer < 2)
+                {
+                    sb.Insert(0, (char)('0' + buffer));
+                    buffer = 0;
+                }
+                else
+                {
+                    sb.Insert(0, (char)('0' + (buffer-2)));
+                    buffer = 1;
+                }
+            }
+            
+            if (buffer > 0)
+                sb.Insert(0, (char)('0' + buffer));
+
+            return sb.ToString();
+        }
     }
 }
