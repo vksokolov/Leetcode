bb57e85b170208a7f54ac8772f7e95a5f396365a
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-05 17:18:27 +0300

    66. Plus One

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -18,11 +18,43 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToPrettyString();
+            int[] arr = { 8,9 };
+            return PlusOne(arr).ToPrettyString();
         }
     }
 
     internal partial class Solution
     {
+        public int[] PlusOne(int[] digits)
+        {
+            if (digits[^1] != 9)
+            {
+                digits[^1]++;
+                return digits;
+            }
+            
+            bool overflow = true;
+
+            
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] != 9)
+                {
+                    overflow = false;
+                    digits[i]++;
+                }
+                else
+                    digits[i] = 0;
+
+                if (!overflow) return digits;
+            }
+
+            if (overflow)
+            {
+                digits = new int[digits.Length + 1];
+                digits[0] = 1;
+            }
+            return digits;
+        }
     }
 }
