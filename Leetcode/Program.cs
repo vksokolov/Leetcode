43f61210ddb33d932d2cffff35b40fb6eba057ee
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-05 19:28:02 +0300

    69. Sqrt(x)

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -18,11 +18,29 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToPrettyString();
+            return MySqrt(int.MaxValue).ToPrettyString();
         }
     }
 
     internal partial class Solution
     {
+        public int MySqrt(int x)
+        {
+            int maxIntSqrt = 46340;
+            int leftBound = 0;
+            int rightBound = x/2;
+
+            if (rightBound > maxIntSqrt)
+                rightBound = maxIntSqrt;
+
+            while (leftBound * leftBound < x && leftBound < maxIntSqrt)
+            {
+                int mid = (leftBound + rightBound) / 2;
+                if (mid * mid > x) rightBound = mid;
+                else leftBound = mid + 1;
+            }
+
+            return leftBound * leftBound <= x ? leftBound : leftBound - 1;
+        }
     }
 }
