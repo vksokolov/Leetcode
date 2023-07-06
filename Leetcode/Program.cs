2ba6eebf62de9a9895bf9812a3c3509a7a444469
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-06 20:13:30 +0300

    100. Same Tree

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -2,6 +2,20 @@
 
 namespace Leetcode
 {
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     class Program
     {
         public static void Main()
@@ -24,5 +38,12 @@ namespace Leetcode
 
     internal partial class Solution
     {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            return (p == null && q == null) ||
+                   p?.val == q?.val &&
+                   IsSameTree(p.left, q.left) &&
+                   IsSameTree(p.right, q.right);
+        }
     }
 }
