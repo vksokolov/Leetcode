c1f0d3908c81bebebf35c3583ee0cb0711667213
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-06 18:52:29 +0300

    83. Remove Duplicates from Sorted List

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,7 +1,38 @@
 ﻿using System;
+using System.Text;
 
 namespace Leetcode
 {
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public override string ToString()
+        {
+            var node = this;
+            var sb = new StringBuilder();
+            
+            sb.Append('[');
+            sb.Append(val);
+            
+            while (node.next != null)
+            {
+                node = node.next;
+                sb.Append(',');
+                sb.Append(' ');
+                sb.Append(node.val);
+            }
+
+            sb.Append(']');
+
+            return sb.ToString();
+        }
+    }
+    
     class Program
     {
         public static void Main()
@@ -18,11 +49,33 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToPrettyString();
+            var head = new ListNode(1, new ListNode(1, new ListNode(1)));
+            return DeleteDuplicates(head).ToPrettyString();
         }
     }
 
     internal partial class Solution
     {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return null;
+            
+            ListNode node = head;
+            while (node.next != null)
+            {
+                if (node.val == node.next.val)
+                {
+                    node.val = node.next.val;
+                    node.next = node.next.next;
+                }
+                else 
+                    node = node.next;
+
+                if (node.next == null) break;
+                
+            }
+
+            return head;
+        }
     }
 }
