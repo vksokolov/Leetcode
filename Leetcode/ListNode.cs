452ab6d6d1566f88439aa3195efd929f322c4235
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-07 00:24:52 +0300

    ListNode

---
+++ Leetcode/ListNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Leetcode;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+
+    public override string ToString()
+    {
+        var node = this;
+        var sb = new StringBuilder();
+            
+        sb.Append('[');
+        sb.Append(val);
+            
+        while (node.next != null)
+        {
+            node = node.next;
+            sb.Append(',');
+            sb.Append(' ');
+            sb.Append(node.val);
+        }
+
+        sb.Append(']');
+
+        return sb.ToString();
+    }
+}
