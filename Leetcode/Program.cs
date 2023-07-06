4ad21b1ad8de4dbe30ea306200fa4ae37f655f5b
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-06 20:21:20 +0300

    101. Symmetric Tree

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -2,6 +2,20 @@
 
 namespace Leetcode
 {
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
     class Program
     {
         public static void Main()
@@ -24,5 +38,18 @@ namespace Leetcode
 
     internal partial class Solution
     {
+        public bool IsSymmetric(TreeNode root)
+        {
+            return IsLeftSymmetricalToRight(root.left, root.right);
+
+            bool IsLeftSymmetricalToRight(TreeNode left, TreeNode right)
+            {
+                if (left == null) return right == null;
+                if (right == null || left.val != right.val) return false;
+                return IsLeftSymmetricalToRight(left.left, right.right) &&
+                       IsLeftSymmetricalToRight(left.right, right.left);
+            }
+
+        }
     }
 }
