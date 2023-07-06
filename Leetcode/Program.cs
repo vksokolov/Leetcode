77bf6b0347d13e6a1306334def66bf822df416b9
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-07 01:31:23 +0300

    3. Longest Substring Without Repeating Characters

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Leetcode
 {
@@ -18,11 +19,32 @@ namespace Leetcode
     {
         public string GetResult()
         {
-            return .ToPrettyString();
+            return LengthOfLongestSubstring("abba").ToPrettyString();
         }
     }
 
     internal partial class Solution
     {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            
+            var set = new Dictionary<char, int>();
+            var leftIndex = 0;
+            var maxLen = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (set.TryGetValue(s[i], out var index))
+                {
+                    if (i - leftIndex > maxLen) maxLen = i - leftIndex;
+                    if (index + 1 > leftIndex) leftIndex = index + 1;
+                }
+                set[s[i]] = i;
+            }
+
+            if (s.Length - leftIndex > maxLen) maxLen = s.Length - leftIndex;
+            return maxLen;
+        }
     }
 }
