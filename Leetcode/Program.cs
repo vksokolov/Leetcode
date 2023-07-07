6ba6abd76f124f4ecf16f0f9f0bac21c52f93e69
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-07 15:37:37 +0300

    Benchmark (ms) added

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Leetcode
 {
@@ -16,9 +17,26 @@ namespace Leetcode
 
     internal partial class Solution
     {
+        public const bool Benchmark = false;
+        private static Stopwatch _watch = new Stopwatch();
+
         public string GetResult()
         {
-            return .ToPrettyString();
+            var func = () => GetRow(4);
+            if (Benchmark)
+            {
+                int iterations = 9999999;
+                _watch.Start();
+
+                for (int i = 0; i < iterations; i++)
+                    func();
+
+                _watch.Stop();
+                return func().ToPrettyString() + $"\n{iterations} iterations, {_watch.ElapsedMilliseconds} ms";
+            }
+
+            var result = func();
+            return result.ToPrettyString();
         }
     }
 
