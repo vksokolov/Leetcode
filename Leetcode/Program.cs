b26512a72692e73b48391d582f3b1bcbc30ffbc9
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-09 21:07:54 +0300

    191. Number of 1 Bits

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -42,5 +42,16 @@ namespace Leetcode
 
     internal partial class Solution
     {
+        public int HammingWeight(uint n)
+        {
+            uint weight = 0;
+            while (n > 0)
+            {
+                weight += (n & 1);
+                n >>= 1;
+            }
+
+            return (int)weight;
+        }
     }
 }
