e7b880b669ed54bf7c4f2390f08911fc9a60736b
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-11 18:39:07 +0300

    228. Summary Ranges

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 
 namespace Leetcode
 {
@@ -42,5 +44,46 @@ namespace Leetcode
 
     internal partial class Solution
     {
+        public IList<string> SummaryRanges(int[] nums)
+        {
+            var result = new List<string>();
+            
+            if (nums.Length == 0) return result;
+
+            var rangeStart = nums[0];
+            var rangeEnd = rangeStart;
+            
+            for (var i = 1; i < nums.Length; i++)
+            {
+                var num = nums[i];
+
+                if (num == rangeEnd + 1)
+                    rangeEnd++;
+                else
+                {
+                    AppendRange(rangeStart, rangeEnd);
+
+                    rangeStart = num;
+                    rangeEnd = num;
+                }
+            }
+            
+            AppendRange(rangeStart, rangeEnd);
+
+            return result;
+
+            void AppendRange(int left, int right) => 
+                result.Add(rangeEnd != rangeStart ? $"{left}->{right}" : $"{left}");
+        }
     }
+}
+
+
+
+
+
+if (rangeStart == rangeEnd && num != ++rangeEnd)
+{
+    rangeStart = rangeEnd;
+}
 }
