5ed7191acb30753ad2f717208a7dde8ba92c0e1b
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-12 19:30:38 +0300

    2423. Remove Letter To Equalize Frequency

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Leetcode
 {
@@ -22,7 +24,7 @@ namespace Leetcode
 
         public string GetResult()
         {
-            var func = () => GetRow(4);
+            var func = () => EqualFrequency("bac");
             if (Benchmark)
             {
                 int iterations = 9999999;
@@ -42,5 +44,51 @@ namespace Leetcode
 
     internal partial class Solution
     {
+        public bool EqualFrequency(string word)
+        {
+            var charFrequency = new Dictionary<char, byte>();
+            var frequencyFrequency = new Dictionary<byte, byte>();
+            
+            foreach (var c in word)
+                if (!charFrequency.TryAdd(c, 1))
+                    charFrequency[c]++;
+
+            foreach (var (_, val) in charFrequency)
+                if (!frequencyFrequency.TryAdd(val, 1))
+                    frequencyFrequency[val]++;
+
+            return
+                AllCharsAreUniqueOrSame() ||
+                frequencyFrequency.Count == 2 &&
+                (HasSingleEntryOfSingleChar() || CanEqualizeByRemovingTheMostFrequentChar());
+
+            bool AllCharsAreUniqueOrSame()
+            {
+                if (frequencyFrequency.Count != 1) return false;
+
+                using var enumerator = frequencyFrequency.GetEnumerator();
+                enumerator.MoveNext();
+                return enumerator.Current.Key == 1 || enumerator.Current.Value == 1;
+            }
+
+            bool HasSingleEntryOfSingleChar() => 
+                frequencyFrequency.TryGetValue(1, out var amount) && amount == 1;
+
+            bool CanEqualizeByRemovingTheMostFrequentChar()
+            {
+                (byte EntryLength, byte Entries) max = (0,0);
+                (byte EntryLength, byte Entries) min = (byte.MaxValue,byte.MaxValue);
+                foreach (var (entryLength, entries) in frequencyFrequency)
+                {
+                    if (entryLength > max.EntryLength) 
+                        max = (entryLength, entries);
+                    
+                    if (entryLength < min.EntryLength) 
+                        min = (entryLength, entries);
+                }
+
+                return max.Entries == 1 && max.EntryLength - min.EntryLength == 1;
+            }
+        }
     }
 }
