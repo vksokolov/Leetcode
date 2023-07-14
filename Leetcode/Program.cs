ae6d43fd657d165c06593650f0261404110038a6
vksokolov <vladislav.k.sokolov@gmail.com>
2023-07-14 20:53:48 +0300

    Improvement: File-scoped namespace

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,46 +1,45 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace Leetcode
+namespace Leetcode;
+
+class Program
 {
-    class Program
+    public static void Main()
     {
-        public static void Main()
+        var s = new Solution();
+        Print(s.GetResult());
+    }
+
+    private static void Print(string msg) =>
+        Console.WriteLine(msg);
+}
+
+internal partial class Solution
+{
+    public const bool Benchmark = false;
+    private static Stopwatch _watch = new Stopwatch();
+
+    public string GetResult()
+    {
+        var func = () => GetRow(4);
+        if (Benchmark)
         {
-            var s = new Solution();
-            Print(s.GetResult());
+            int iterations = 9999999;
+            _watch.Start();
+
+            for (int i = 0; i < iterations; i++)
+                func();
+
+            _watch.Stop();
+            return func().ToPrettyString() + $"\n{iterations} iterations, {_watch.ElapsedMilliseconds} ms";
         }
 
-        private static void Print(string msg) =>
-            Console.WriteLine(msg);
+        var result = func();
+        return result.ToPrettyString();
     }
+}
 
-    internal partial class Solution
-    {
-        public const bool Benchmark = false;
-        private static Stopwatch _watch = new Stopwatch();
-
-        public string GetResult()
-        {
-            var func = () => GetRow(4);
-            if (Benchmark)
-            {
-                int iterations = 9999999;
-                _watch.Start();
-
-                for (int i = 0; i < iterations; i++)
-                    func();
-
-                _watch.Stop();
-                return func().ToPrettyString() + $"\n{iterations} iterations, {_watch.ElapsedMilliseconds} ms";
-            }
-
-            var result = func();
-            return result.ToPrettyString();
-        }
-    }
-
-    internal partial class Solution
-    {
-    }
+internal partial class Solution
+{
 }
