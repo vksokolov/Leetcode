35a3d9315a18dd2555bd11222d0ada866fcb1863
vksokolov <vladislav.k.sokolov@gmail.com>
2023-08-17 18:41:43 +0300

    1476. Subrectangle Queries

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Leetcode;
@@ -42,4 +43,50 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    public class SubrectangleQueries
+    {
+        private int[][] _rectangle;
+        private List<RectQuery> _queries = new();
+        
+        public SubrectangleQueries(int[][] rectangle)
+        {
+            _rectangle = rectangle;
+        }
+
+        public void UpdateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+        {
+            _queries.Add(new RectQuery(row1, col1, row2, col2, newValue));
+        }
+
+        public int GetValue(int row, int col)
+        {
+            RectQuery lastQuery = null;
+            foreach (var query in _queries)
+            {
+                if (query.IsInside(row, col))
+                    lastQuery = query;
+            }
+
+            return lastQuery?.Value ?? _rectangle[row][col];
+        }
+
+        private class RectQuery
+        {
+            public readonly (int x, int y) Start;
+            public readonly (int x, int y) End;
+            public readonly int Value;
+
+            public RectQuery(int row1, int col1, int row2, int col2, int value)
+            {
+                Start.x = row1;
+                Start.y = col1;
+                End.x = row2;
+                End.y = col2;
+                Value = value;
+            }
+            
+            public bool IsInside(int row, int col) => 
+                row >= Start.x && row <= End.x && col >= Start.y && col <= End.y;
+        }
+    }
 }
