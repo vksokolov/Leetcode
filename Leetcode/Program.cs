124e986880d8d30be185e326ea8478fc44006ec0
vksokolov <vladislav.k.sokolov@gmail.com>
2023-08-17 19:10:15 +0300

    1828. Queries on Number of Points Inside a Circle

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -42,4 +42,36 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    public int[] CountPoints(int[][] points, int[][] queries)
+    {
+        var result = new int[queries.Length];
+
+        for (var queryIndex = 0; queryIndex < queries.Length; queryIndex++)
+        {
+            var query = queries[queryIndex];
+            var rSqr = query[2] * query[2];
+            foreach (var point in points)
+            {
+                if (point[0] - query[0] + point[1] - query[1] <= rSqr)
+                    result[queryIndex]++;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] CountPoints2(int[][] points, int[][] queries)
+    {
+        var res = new int[queries.Length];
+        for (var i = 0; i < queries.Length; i++)
+        {
+            var query = queries[i];
+            var r = query[2] * query[2];
+            foreach (var point in points)
+                if ((point[0] - query[0]) * (point[0] - query[0]) + (point[1] - query[1]) * (point[1] - query[1]) <= r)
+                    res[i]++;
+        }
+
+        return res;
+    }
 }
