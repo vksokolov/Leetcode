e5aba4e32acdf03d62a1c891bd008e16ee366bff
vksokolov <vladislav.k.sokolov@gmail.com>
2023-08-21 17:47:59 +0300

    6. Zigzag Conversion

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -22,7 +22,7 @@ internal partial class Solution
 
     public string GetResult()
     {
-        var func = () => GetRow(4);
+        var func = () => Convert("PAYPALISHIRING", 4);
         if (Benchmark)
         {
             int iterations = 9999999;
@@ -42,4 +42,34 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    public string Convert(string s, int numRows)
+    {
+        if (numRows == 1) return s;
+        var mid = numRows - 1;
+        var charsInLoop = mid << 1;
+        var i = 0;
+        Span<char> result = stackalloc char[s.Length];
+        for (var row = 0; row < numRows; row++)
+        {
+            var index = row;
+            var doubleDistanceToMid = (mid - row) << 1;
+            
+            while (index < s.Length)
+            {
+                result[i++] = s[index];
+                if (row == 0 || row == mid)
+                    index += charsInLoop;
+                else
+                {
+                    var res = index % charsInLoop;
+                    if (res > mid)
+                        index += charsInLoop - doubleDistanceToMid;
+                    else
+                        index += doubleDistanceToMid;
+                }
+            }
+        }
+        
+        return new string(result);
+    }
 }
