4fe6161d9cd39581ffde1f3d90fb4ec9487a0aa2
vksokolov <vladislav.k.sokolov@gmail.com>
2023-08-22 16:45:30 +0300

    8. String to Integer (atoi)

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -22,7 +22,7 @@ internal partial class Solution
 
     public string GetResult()
     {
-        var func = () => GetRow(4);
+        var func = () => MyAtoi("-2147483648");
         if (Benchmark)
         {
             int iterations = 9999999;
@@ -42,4 +42,52 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    public int MyAtoi(string s)
+    {
+        bool? isPositive = default;
+        var digitFound = false;
+        var result = 0L;
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case ' ':
+                    if (digitFound || isPositive.HasValue)
+                        return GetSignedResult();
+                    break;
+                case '-' or '+':
+                    if (digitFound || isPositive.HasValue)
+                        return GetSignedResult();
+                    
+                    isPositive = c == '+';
+                    break;
+                case <'0' or >'9':
+                    return GetSignedResult();
+                default:
+                {
+                    var digit = c - '0';
+                    if (digit is <10 and >= 0)
+                    {
+                        digitFound = true;
+                        result = result * 10 + digit;
+                        
+                        if (isPositive.HasValue && !isPositive.Value && -result <= int.MinValue)
+                            return int.MinValue;
+                        
+                        if (result > int.MaxValue)
+                            return int.MaxValue;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return GetSignedResult();
+        
+        int GetSignedResult()
+        {
+            isPositive ??= true;
+            return (int)(isPositive.Value ? result : -result);
+        }
+    }
 }
