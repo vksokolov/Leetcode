c33cbe62388af917ef7e5eadc6f5706a5b739abc
vksokolov <vladislav.k.sokolov@gmail.com>
2023-08-23 17:20:54 +0300

    15. 3Sum

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Leetcode;
 
@@ -22,7 +24,7 @@ internal partial class Solution
 
     public string GetResult()
     {
-        var func = () => GetRow(4);
+        var func = () => ThreeSum(new int[]{-1,0,1,2,-1,-4});
         if (Benchmark)
         {
             int iterations = 9999999;
@@ -42,4 +44,37 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        var set = new HashSet<(int i, int j, int k)>();
+        var map = new Dictionary<int, List<int>>()
+        {
+            {nums[0], new List<int> {0}},
+        };
+        for (var i = 1; i < nums.Length-1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                var negativeSum = -(nums[i] + nums[j]);
+
+                if (map.TryGetValue(negativeSum, out var kIndexes))
+                {
+                    var entryValues = new [] { nums[i], nums[j], negativeSum };
+                    Array.Sort(entryValues);
+                    set.Add((entryValues[0], entryValues[1], entryValues[2]));
+                }
+            }
+            
+            if (!map.TryGetValue(nums[i], out var indexes))
+                map.Add(nums[i], indexes = new List<int> {i});
+
+            indexes.Add(i);
+        }
+
+        foreach (var (i, j, k) in set)
+            result.Add(new List<int> {i, j, k});
+        
+        return result;
+    }
 }
