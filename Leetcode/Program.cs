7495f9db537046c2086a745facaf0e49c8b99672
vksokolov <vladislav.k.sokolov@gmail.com>
2023-08-23 22:39:27 +0300

    17. Letter Combinations of a Phone Number

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -42,4 +42,43 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    public IList<string> LetterCombinations(string digits) {
+        var result = new List<string>();
+        if (string.IsNullOrEmpty(digits))
+            return result;
+
+        var map = new Dictionary<char, char[]>()
+        {
+            { '2', new[] { 'a', 'b', 'c' } },
+            { '3', new[] { 'd', 'e', 'f' } },
+            { '4', new[] { 'g', 'h', 'i' } },
+            { '5', new[] { 'j', 'k', 'l' } },
+            { '6', new[] { 'm', 'n', 'o' } },
+            { '7', new[] { 'p', 'r', 's', 'q' } },
+            { '8', new[] { 't', 'u', 'v' } },
+            { '9', new[] { 'w', 'x', 'y', 'z' } },
+        };
+
+        for (var index = 0; index < digits.Length; index++)
+        {
+            var digit = digits[index];
+            if (!map.TryGetValue(digit, out var symbols))
+                continue;
+
+            if (result.Count == 0)
+                foreach (var c in symbols)
+                    result.Add(c.ToString());
+            else
+            {
+                var initCount = result.Count;
+                foreach (var c in symbols)
+                    for (var i = 0; i < initCount; i++)
+                        result.Add(result[i] + c);
+            }
+        }
+
+        result.RemoveAll(x => x.Length != digits.Length);
+        
+        return result;
+    }
 }
