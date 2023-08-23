cbb1e6d01ab2e7818702b9d1a22d91fc0e077209
vksokolov <vladislav.k.sokolov@gmail.com>
2023-08-23 22:45:07 +0300

    Solution using StringBuilder

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -60,16 +60,45 @@ internal partial class Solution
             "tuv",
             "wxyz",
         };
-        
-        var symbolSequences = digits.Select(d => map[d - '2']).ToArray();
-        var combinations = GetAllCombinations1(symbolSequences, digits.Length-1);
-        
-        return combinations.Select(x => new string(x.ToArray())).ToList();
 
-        IEnumerable<IEnumerable<char>> GetAllCombinations1(IReadOnlyList<IEnumerable<char>> sequences, int sequenceIndex) =>
-            sequenceIndex == 0
-                ? sequences[0].Select(c => new[] { c })
-                : GetAllCombinations1(sequences, sequenceIndex - 1)
-                    .SelectMany(_ => sequences[sequenceIndex], (chars, c) => chars.Append(c));
+        var sb = new StringBuilder(map[digits[0] - '2']);
+        var combinationLength = 1;
+        var totalCombinations = map[digits[0] - '2'].Length;
+
+        for (var index = 1; index < digits.Length; index++)
+        {
+            var digit = digits[index];
+            string chars = map[digit - '2'];
+            int initialCombinationLength = combinationLength++;
+            
+            // sb = "abc", chars = "def"
+            // sb => "abcabcabc"
+            // (abc abc abc)
+            //  (d   e   f)
+            var sbLength = sb.Length;
+            for (var i = 0; i < chars.Length - 1; i++)
+            for (var j = 0; j < sbLength; j++)
+                sb.Append(sb[j]);
+            
+            // sb = "abcabcabc", chars = "def"
+            // sb => "adbdcdaebeceafbfcf"
+            // (ad bd cd    ae be ce    af bf cf)
+            var index1 = initialCombinationLength;
+            var combinations = totalCombinations;
+            totalCombinations = 0;
+            for (var j1 = 0; j1 < chars.Length; j1++)
+            for (var i1 = 0; i1 < combinations; i1++)
+            {
+                sb.Insert(index1, chars[j1]);
+                index1 += initialCombinationLength + 1;
+                totalCombinations++;
+            }
+        }
+
+        result = new List<string>(totalCombinations);
+        for(var i=0;i<totalCombinations;i++) 
+            result.Add(sb.ToString(i * combinationLength, combinationLength));
+
+        return result;
     }
 }
