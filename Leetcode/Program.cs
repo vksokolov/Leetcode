b7244320fe99f284da71cf17ab9cabf24e3ec8d4
vksokolov <vladislav.k.sokolov@gmail.com>
2023-09-04 18:06:09 +0300

    95. Unique Binary Search Trees II

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Leetcode;
 
@@ -22,7 +24,7 @@ internal partial class Solution
 
     public string GetResult()
     {
-        var func = () => GetRow(4);
+        var func = () => GenerateTrees(3);
         if (Benchmark)
         {
             int iterations = 9999999;
@@ -42,4 +44,27 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    private const TreeNode NullNode = null;
+    public IList<TreeNode> GenerateTrees(int n) => GenerateTrees(1, n);
+
+    public IList<TreeNode> GenerateTrees(int left, int right)
+    {
+        if (left > right) return new List<TreeNode> { NullNode };
+
+        var result = new List<TreeNode>();
+        
+        for (var i = left; i <= right; i++)
+        {
+            //generate left trees
+            var leftTrees = GenerateTrees(left, i-1);
+            //generate right trees
+            var rightTrees = GenerateTrees(i+1, right);
+
+            foreach (var leftTree in leftTrees)
+            foreach (var rightTree in rightTrees)
+                result.Add(new TreeNode(i, leftTree, rightTree));
+        }
+
+        return result;
+    }
 }
