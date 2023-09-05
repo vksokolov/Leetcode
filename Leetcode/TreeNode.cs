229d9ad3134a89979fd1b5eb89fe53004710575a
vksokolov <vladislav.k.sokolov@gmail.com>
2023-09-05 18:18:23 +0300

    TreeNode.Create(int?[]) added

--- Leetcode/TreeNode.cs
+++ Leetcode/TreeNode.cs
@@ -1,4 +1,6 @@
-﻿namespace Leetcode;
+﻿using System.Collections.Generic;
+
+namespace Leetcode;
 
 public class TreeNode
 {
@@ -11,5 +13,40 @@ public class TreeNode
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static TreeNode Create(int?[] inputArray)
+    {
+
+        if (inputArray.Length == 0) return null;
+
+        var index = 0;
+        var rootValue = inputArray[index++];
+        if (rootValue == null) return null;
+
+        var root = new TreeNode(rootValue.Value);
+        var nodeQueue = new Queue<TreeNode>();
+        nodeQueue.Enqueue(root);
+
+        while (nodeQueue.Count > 0 && index < inputArray.Length)
+        {
+            var curNode = nodeQueue.Dequeue();
+            var leftVal = index < inputArray.Length ? inputArray[index++] : null;
+            var rightVal = index < inputArray.Length ? inputArray[index++] : null;
+
+            if (leftVal != null)
+            {
+                curNode.left = new TreeNode(leftVal.Value);
+                nodeQueue.Enqueue(curNode.left);
+            }
+
+            if (rightVal != null)
+            {
+                curNode.right = new TreeNode(rightVal.Value);
+                nodeQueue.Enqueue(curNode.right);
+            }
+        }
+
+        return root;
     }
 }
