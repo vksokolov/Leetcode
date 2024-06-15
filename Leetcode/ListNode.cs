bdfab9804c04c8ef9ad5e99d6888e5e9708bfc37
vksokolov <vladislav.k.sokolov@gmail.com>
2024-06-15 13:51:04 +0700

    ListNode.Create(int[]) added

--- Leetcode/ListNode.cs
+++ Leetcode/ListNode.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Leetcode;
 
@@ -32,5 +33,32 @@ public class ListNode
         sb.Append(']');
 
         return sb.ToString();
+    }
+    
+    public static ListNode Create(int?[] inputArray)
+    {
+        if (inputArray.Length == 0) return null;
+
+        var index = 0;
+        var rootValue = inputArray[index++];
+        if (rootValue == null) return null;
+
+        var root = new ListNode(rootValue.Value);
+        var nodeQueue = new Queue<ListNode>();
+        nodeQueue.Enqueue(root);
+
+        while (nodeQueue.Count > 0 && index < inputArray.Length)
+        {
+            var curNode = nodeQueue.Dequeue();
+            var nextVal = index < inputArray.Length ? inputArray[index++] : null;
+
+            if (nextVal != null)
+            {
+                curNode.next = new ListNode(nextVal.Value);
+                nodeQueue.Enqueue(curNode.next);
+            }
+        }
+
+        return root;
     }
 }
