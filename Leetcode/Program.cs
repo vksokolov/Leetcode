20106d2ae601b11d959e53ea62eb3b27514e59ae
vksokolov <vladislav.k.sokolov@gmail.com>
2024-06-17 13:35:15 +0700

    Iteration 1

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -42,4 +42,17 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    // Encodes a URL to a shortened URL
+    public string encode(string longUrl) 
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(longUrl);
+        return System.Convert.ToBase64String(plainTextBytes);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public string decode(string shortUrl) 
+    {
+        var base64EncodedBytes = System.Convert.FromBase64String(shortUrl);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
 }
