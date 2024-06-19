2bc03684e66531589fdedc3e5c4d1d0cce6d0f7a
vksokolov <vladislav.k.sokolov@gmail.com>
2024-06-19 20:00:28 +0700

    Iteration 1

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -22,7 +22,7 @@ internal partial class Solution
 
     public string GetResult()
     {
-        var func = () => GetRow(4);
+        var func = () => MinOperations("001011");
         if (Benchmark)
         {
             int iterations = 9999999;
@@ -42,4 +42,26 @@ internal partial class Solution
 
 internal partial class Solution
 {
+    public int[] MinOperations(string boxes) {
+        // got forward and backward
+        
+        var n = boxes.Length;
+        var turnsToMoveFromTheLeft = new int[n];
+        var turnsToMoveFromTheRight = new int[n];
+        var ballsOnTheLeft = 0;
+        var ballsOnTheRight = 0;
+        for (var i = 1; i < n; i++)
+        {
+            ballsOnTheLeft += boxes[i - 1] - '0';
+            turnsToMoveFromTheLeft[i] += turnsToMoveFromTheLeft[i-1] + ballsOnTheLeft;
+            
+            ballsOnTheRight += boxes[n - i] - '0';
+            turnsToMoveFromTheRight[n - i - 1] += turnsToMoveFromTheRight[n - i] + ballsOnTheRight;
+        }
+        
+        for (var i = 0; i < n; i++)
+            turnsToMoveFromTheLeft[i] += turnsToMoveFromTheRight[i];
+        
+        return turnsToMoveFromTheLeft;
+    }
 }
